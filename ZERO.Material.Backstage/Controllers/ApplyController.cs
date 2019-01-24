43068481cff9d713a38d4bd87a3c22c0b737c5d5
ZERO <1378322133@qq.com>
2019-01-24 15:53:32 +0800

    2019-01-24 批量审批以及筛选完成

--- ZERO.Material.Backstage/Controllers/ApplyController.cs
+++ ZERO.Material.Backstage/Controllers/ApplyController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.Ajax.Utilities;
@@ -8,6 +9,7 @@ using Newtonsoft.Json;
 using ZERO.Material.Command;
 using ZERO.Material.IBll;
 using ZERO.Material.Model;
+using ZERO.Material.Model.Other;
 
 namespace ZERO.Material.Backstage.Controllers
 {
@@ -29,12 +31,37 @@ namespace ZERO.Material.Backstage.Controllers
         // GET: Apply
         public ActionResult Borrow()
         {
+            List<string> teacherIds = _applyBll.GetEntities(m => true).Select(m => m.Teacher_Id).Distinct().ToList();
+            for (int i = 0; i < teacherIds.Count; i++)
+            {
+                var id = teacherIds[i];
+                teacherIds[i] += "/" + _teacherBll.GetEntity(m => m.Teacher_Id == id).Teacher_Name;
+            }
+
+            ViewBag.teachers = teacherIds;
             return View();
         }
 
-        public string GetMaterialBorrow(int page, int limit)
+        public string GetMaterialBorrow(int page, int limit, ApplyFilter applyFilter)
         {
-            List<Material_Apply> materApplies = _applyBll.GetEntities(m => true).Skip((page - 1) * limit).Take(limit).ToList();
+            DateTime[] applyTimes = applyFilter.GetStartAndEndTime(applyFilter.ApplyTime);
+            DateTime? applyTime0 = applyTimes?[0];
+            DateTime? applyTime1 = applyTimes?[1];
+            DateTime[] startTimes = applyFilter.GetStartAndEndTime(applyFilter.StartTime);
+            DateTime? startTime0 = startTimes?[0];
+            DateTime? startTime1 = startTimes?[1];
+            DateTime[] endTimes = applyFilter.GetStartAndEndTime(applyFilter.EndTime);
+            DateTime? endTime0 = endTimes?[0];
+            DateTime? endTime1 = endTimes?[1];
+            int? status = applyFilter.Status == null ? (int?)null : Int32.Parse(applyFilter.Status);
+            List<Material_Apply> materApplies = _applyBll.GetEntities(m =>
+                (applyFilter.Teacher == null || m.Teacher_Id == applyFilter.Teacher) &&
+                (status == null || m.Head_Aduit == status) &&
+                ((applyTime0 == null || applyTime1 == null) || m.Apply_Time >= applyTime0 && m.Apply_Time <= applyTime1) &&
+                ((startTime0 == null || startTime1 == null) || m.Start_Time >= startTime0 && m.Start_Time <= startTime1) &&
+                ((endTime0 == null || endTime1 == null) || m.End_Time >= endTime0 && m.End_Time <= endTime1))
+                .Skip((page - 1) * limit).Take(limit).ToList();
+
             foreach (Material_Apply materApply in materApplies)
             {
                 Material_Teacher teacher = _teacherBll.GetEntity(m => m.Teacher_Id == materApply.Teacher_Id);
@@ -56,9 +83,9 @@ namespace ZERO.Material.Backstage.Controllers
 
         #endregion 申请列表
 
-        public ActionResult Review(Material_Apply apply)
+        public ActionResult Review(List<Material_Apply> applies)
         {
-            if (_applyBll.UpdateEntities(new List<Material_Apply>() { apply }))
+            if (_applyBll.UpdateEntities(applies))
             {
                 return Content("OK");
             }
---
+++ ZERO.Material.Model/Other/ApplyFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ZERO.Material.Model.Other
+{
+    public class ApplyFilter
+    {
+        public string Teacher { get; set; }
+        public string ApplyTime { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Status { get; set; }
+
+        public DateTime[] GetStartAndEndTime(string times)
+        {
+            if (times == null)
+            {
+                return null;
+            }
+
+            string[] timeStrings = Regex.Split(times, " - ", RegexOptions.IgnoreCase);
+            if (timeStrings.Length == 0)
+            {
+                return null;
+            }
+
+            if (DateTime.TryParse(timeStrings[0], out DateTime start) && DateTime.TryParse(timeStrings[1], out DateTime end))
+            {
+                return new DateTime[]
+                {
+                    start,end
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
